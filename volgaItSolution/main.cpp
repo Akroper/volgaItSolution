94b65e729179bba0182858e006b2f0e5e2456660
Dneprovskiy Dmitriy <alanwake0101@yandex.ru>
2022-03-18 12:09:13 +0400

    moved all files to one folder

--- volgaItSolution/main.cpp
+++ volgaItSolution/main.cpp
@@ -1,6 +1,9 @@
-#include "volgaIt/fairy_tail.hpp"
+#include "fairy_tail.hpp"
+#include "fairy_tail.cpp"
 #include <cstdlib>
 #include <ctime>
+
+using namespace std;
 
 int walk()
 {
@@ -39,11 +42,12 @@ int walk()
 int main()
 {
     srand(static_cast<unsigned int>(time(nullptr)));
-
-    if (const int turns = walk())
-        std::cout << "Found in " << turns << " turns" << std::endl;
-    else
-        std::cout << "Not found" << std::endl;
+    Fairyland world;
+    cout << world.canGo(Character::Ivan, Direction::Up);
+    //if (const int turns = walk())
+    //    std::cout << "Found in " << turns << " turns" << std::endl;
+    //else
+    //    std::cout << "Not found" << std::endl;
 
     return 0;
 }
