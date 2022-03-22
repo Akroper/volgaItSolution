e19b3bcc502cfe0804169c233fca9b0a9d203878
Dneprovskiy Dmitriy <alanwake0101@yandex.ru>
2022-03-22 18:21:16 +0400

    added fixation of origin position of Ivan and Elena

--- volgaItSolution/main.cpp
+++ volgaItSolution/main.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 
 using namespace std;
-
+/*
 int walk()
 {
     Fairyland world;
@@ -37,14 +37,22 @@ int walk()
 
     return 0;
 }
-
+*/
 int main()
 {
-    char[10][10] worldMap;
-    pair<int, int>(0, 0) ivanPos;
-    pair<int, int>(0, 0) elenaPos;
+    
+    pair<int, int> ivanPos (0, 0);
+    pair<int, int> elenaPos (0, 0);
     srand(static_cast<unsigned int>(time(nullptr)));
     Fairyland world;
+    if (world.canGo(Character::Ivan, Direction::Left))ivanPos.first = 0;
+    else if (world.canGo(Character::Ivan, Direction::Right))ivanPos.first = 9;
+    if (world.canGo(Character::Ivan, Direction::Up))ivanPos.second = 0;
+    else if (world.canGo(Character::Ivan, Direction::Down))ivanPos.second = 9;
+    if (world.canGo(Character::Elena, Direction::Left))elenaPos.first = 0;
+    else if (world.canGo(Character::Elena, Direction::Right))elenaPos.first = 9;
+    if (world.canGo(Character::Elena, Direction::Up))elenaPos.second = 0;
+    else if (world.canGo(Character::Elena, Direction::Down))elenaPos.second = 9;
     cout << world.canGo(Character::Ivan, Direction::Down);
     return 0;
 }
